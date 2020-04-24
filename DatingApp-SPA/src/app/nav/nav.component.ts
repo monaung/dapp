9d749dc59db7a494e01b6ffac16f785288efc1f6
monaung <mon.aung@gmail.com>
2020-04-24 23:15:34 +0800

    end of section 6

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
+import { AlertifyService } from '../_services/alertify.service';
 
 @Component({
   selector: 'app-nav',
@@ -7,34 +8,31 @@ import { AuthService } from '../_services/auth.service';
   styleUrls: ['./nav.component.css']
 })
 export class NavComponent implements OnInit {
-model: any = {};
-  constructor(private authService: AuthService) { }
+  model: any = {};
+  constructor(
+    public authService: AuthService,
+    private alertify: AlertifyService
+  ) {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
-  login(){
-      this.authService.login(this.model).subscribe(next => {
-        console.log('Logged in Successfully');
+  login() {
+    this.authService.login(this.model).subscribe(
+      (next) => {
+        this.alertify.success('logged in successfully');
       },
-       error => {
-        console.log(error);
-       }
-      );
+      (error) => {
+        this.alertify.error('log in failed');
+      }
+    );
   }
 
-  loggedIn(){
-
-     const token = localStorage.getItem('token');
-     return !!token;
-
+  loggedIn() {
+    return this.authService.loggedIn();
   }
 
-  logout(){
-
+  logout() {
     localStorage.removeItem('token');
-    console.log('logged out');
-
+    this.alertify.message('logged out successfully');
   }
-
 }
--- DatingApp-SPA/src/app/register/register.component.ts
+++ DatingApp-SPA/src/app/register/register.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
-
+import { AlertifyService } from '../_services/alertify.service';
 
 @Component({
   selector: 'app-register',
@@ -13,25 +13,25 @@ export class RegisterComponent implements OnInit {
   @Output() cancelRegister = new EventEmitter();
   model: any = {};
 
-  constructor(private authService: AuthService) { }
+  constructor(
+    private authService: AuthService,
+    private alertify: AlertifyService
+  ) {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  register() {
+    this.authService.register(this.model).subscribe(
+      () => {
+        this.alertify.success('registration successful');
+      },
+      (error) => {
+        this.alertify.error('registration failed');
+      }
+    );
   }
 
-  register(){
-
-    this.authService.register(this.model).subscribe(() => {
-      console.log('registration successful');
-    }, error => {
-      console.log(error);
-    });
-
+  cancel() {
+    this.cancelRegister.emit(false); //it can emit object, property, etc to parent component
   }
-
-  cancel(){
-
-    this.cancelRegister.emit(false); //it can emit object, property, etc to parent component 
-
-  }
-
 }
